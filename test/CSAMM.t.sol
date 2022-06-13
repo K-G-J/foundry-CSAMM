b20892d2e20bb030591503d59821dab96e7cb07c
Kate Johnson <johnson.kat.g@gmail.com>
2022-06-12 20:33:43 -0400

    initial contracts

--- test/CSAMM.t.sol
+++ test/CSAMM.t.sol
@@ -2,8 +2,11 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/Vm.sol";
+import "solmate/test/utils/mocks/MockERC20.sol";
+import "../src/CSAMM.sol";
 
-contract ContractTest is Test {
+contract CSAMMTest is Test {
     function setUp() public {}
 
     function testExample() public {
