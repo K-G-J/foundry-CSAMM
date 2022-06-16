160102d4ed98c6580a903f271b331cdc8a3d9636
Kate Johnson <johnson.kat.g@gmail.com>
2022-06-15 23:21:00 -0400

    passing fuzz test for swapping

--- test/CSAMM.t.sol
+++ test/CSAMM.t.sol
@@ -26,9 +26,9 @@ contract CSAMMTest is Test {
 
         CSAMMcontract = new CSAMM(address(token0), address(token1));
 
-        token0.mint(address(this), 1e18);
+        token0.mint(address(this), 100);
         token0.approve(address(CSAMMcontract), 100);
-        token1.mint(address(this), 1e18);
+        token1.mint(address(this), 100);
         token1.approve(address(CSAMMcontract), 100);
     }
 
@@ -98,8 +98,8 @@ contract CSAMMTest is Test {
         CSAMMcontract.addLiquidity(100, 100);
         uint _shares = CSAMMcontract.balanceOf(address(this));
         CSAMMcontract.removeLiquidity(_shares);
-        assertEq(token0.balanceOf(address(this)), 1e18);
-        assertEq(token1.balanceOf(address(this)), 1e18);
+        assertEq(token0.balanceOf(address(this)), 100);
+        assertEq(token1.balanceOf(address(this)), 100);
         assertEq(CSAMMcontract.balanceOf(address(this)), 0);
         assertEq(CSAMMcontract.totalSupply(), 0);
         assertEq(CSAMMcontract.reserve0(), 0);
@@ -128,28 +128,53 @@ contract CSAMMTest is Test {
         vm.assume(swapAmount <= token0.totalSupply());
         vm.assume(swapAmount <= token1.totalSupply());
         vm.assume(swapAmount <= token0.balanceOf(address(this)));
-        // vm.assume(swapAmount <= 100);
 
-        token0.mint(address(this), 1e18);
-        token1.mint(address(this), 1e18);
+        token0.mint(address(this), 100);
+        token1.mint(address(this), 100);
         token0.approve(address(CSAMMcontract), 200);
         token1.approve(address(CSAMMcontract), 200);
-
         CSAMMcontract.addLiquidity(100, 100);
-        console.log(token0.balanceOf(address(this)));
         uint token0BalBefore = token0.balanceOf(address(this));
         uint token1BalBefore = token1.balanceOf(address(this));
         uint res0prebal = CSAMMcontract.reserve0();
         uint res1prebal = CSAMMcontract.reserve1();
-
         token0.approve(address(CSAMMcontract), swapAmount);
-        token1.approve(address(CSAMMcontract), swapAmount);
         CSAMMcontract.swap(address(token0), swapAmount);
+        assertEq(CSAMMcontract.reserve0(), res0prebal + swapAmount);
+        assertEq(token0.balanceOf(address(this)), token0BalBefore - swapAmount);
+        uint amountOut = uint(swapAmount * 997) / 1000;
+        assertEq(CSAMMcontract.reserve1(), res1prebal - amountOut);
+        assertEq(token1.balanceOf(address(this)), token1BalBefore + amountOut);
+    }
 
-        // assertEq(CSAMMcontract.reserve0(), res0prebal + swapAmount);
-        // assertEq(token0.balanceOf(address(this)), token0BalBefore - swapAmount);
-        // uint amountOut = uint(swapAmount * 997) / 1000;
-        // assertEq(CSAMMcontract.reserve1(), res1prebal - amountOut);
-        // assertEq(token1.balanceOf(address(this)), token1BalBefore + amountOut);
+    function testFuzz__multiSwap(uint256 swapAmount0, uint256 swapAmount1)
+        public
+    {
+        vm.assume(swapAmount0 > 0 && swapAmount1 > 0);
+        vm.assume(swapAmount0 <= token0.totalSupply());
+        vm.assume(swapAmount1 <= token1.totalSupply());
+        vm.assume(swapAmount0 <= token0.balanceOf(address(this)));
+        vm.assume(swapAmount1 <= token1.balanceOf(address(this)));
+
+        token0.mint(address(this), 100);
+        token1.mint(address(this), 100);
+        token0.approve(address(CSAMMcontract), 200);
+        token1.approve(address(CSAMMcontract), 200);
+        CSAMMcontract.addLiquidity(100, 100);
+        token0.approve(address(CSAMMcontract), swapAmount0);
+        // first swap
+        CSAMMcontract.swap(address(token0), swapAmount0);
+        uint token0BalBefore = token0.balanceOf(address(this));
+        uint token1BalBefore = token1.balanceOf(address(this));
+        uint res0prebal = CSAMMcontract.reserve0();
+        uint res1prebal = CSAMMcontract.reserve1();
+        uint amountOut = (swapAmount1 * 997) / 1000;
+        token1.approve(address(CSAMMcontract), swapAmount1);
+        // second swap
+        CSAMMcontract.swap(address(token1), swapAmount1);
+        assertEq(token1.balanceOf(address(this)), token1BalBefore - swapAmount1);
+        assertEq(CSAMMcontract.reserve1(), res1prebal + swapAmount1);
+        assertEq(token0.balanceOf(address(this)), token0BalBefore + amountOut);
+        assertEq(CSAMMcontract.reserve0(), res0prebal - amountOut);
     }
 }
